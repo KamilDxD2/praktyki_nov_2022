644e1f8cdf016b0c9c704fefef7190a5163c2681
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-11-21 14:05:40 +0100

    Switch to Bluetooth Low Energy.

--- main/src/bluetooth/main.c
+++ main/src/bluetooth/main.c
@@ -6,56 +6,112 @@
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
 #include "esp_bt.h"
-#include "esp_gap_bt_api.h"
+#include "esp_gap_ble_api.h"
+#include "esp_hidd_prf_api.h"
 
 #include "main.h"
-#include "hid.h"
 #include "../debug.h"
 
-static const char *default_pin = "1234123412341234";
+static uint8_t hidd_service_uuid128[] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x12, 0x18, 0x00, 0x00,
+};
+
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x03c0,       //HID Generic,
+    .manufacturer_len = 0,
+    .p_manufacturer_data =  NULL,
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(hidd_service_uuid128),
+    .p_service_uuid = hidd_service_uuid128,
+    .flag = 0x6,
+};
+
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x30,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr            =
+    //.peer_addr_type       =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy  = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
 
 static bool initialized = false;
 static char* device_name = NULL;
+static bluetooth_device_state_cb_t device_state_cb = NULL;
 
-static void bluetooth_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param) {
+static void bluetooth_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
     switch (event) {
-    case ESP_BT_GAP_AUTH_CMPL_EVT:
-        if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
-            DT_INFO("Authentication completed with device %s", param->auth_cmpl.device_name);
-        } else {
-            DT_WARNING("Authentication failed with device %s with error code %d", param->auth_cmpl.device_name, param->auth_cmpl.stat);
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+        DT_INFO("Starting advertising");
+        esp_ble_gap_start_advertising(&adv_params);
+        break;
+    case ESP_GAP_BLE_SEC_REQ_EVT:
+        DT_INFO("BLE security request from " DT_BT_ADDR_FORMAT, DT_BT_ADDR(param->ble_security.ble_req.bd_addr));
+        esp_ble_gap_security_rsp(param->ble_security.ble_req.bd_addr, true);
+        break;
+    case ESP_GAP_BLE_AUTH_CMPL_EVT:
+        DT_INFO("BLE authentication completed with " DT_BT_ADDR_FORMAT, DT_BT_ADDR(param->ble_security.auth_cmpl.bd_addr));
+        DT_INFO("Pair status: %s", param->ble_security.auth_cmpl.success ? "success" : "fail");
+        if (!param->ble_security.auth_cmpl.success) {
+            DT_INFO("Error code: %d", param->ble_security.auth_cmpl.fail_reason);
         }
-        break;
-    case ESP_BT_GAP_PIN_REQ_EVT: {
-        const int pin_length = param->pin_req.min_16_digit ? 16 : 4;
-        esp_bt_pin_code_t pin_code;
-        memcpy(pin_code, default_pin, pin_length);
-        DT_INFO("Use code: %.*s for pairing with " DT_BT_ADDR_FORMAT, pin_length, pin_code, DT_BT_ADDR(param->pin_req.bda));
-        esp_bt_gap_pin_reply(param->pin_req.bda, true, pin_length, pin_code);
-        break;
-    }
-#ifdef CONFIG_BT_SSP_ENABLED
-    case ESP_BT_GAP_CFM_REQ_EVT:
-        DT_INFO("Please compare the PIN code: %d", param->cfm_req.num_val);
-        esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
-        break;
-    case ESP_BT_GAP_KEY_NOTIF_EVT:
-        DT_INFO("Use this passkey to connect: %d", param->key_notif.passkey);
-        break;
-    case ESP_BT_GAP_KEY_REQ_EVT:
-        DT_INFO("Please enter the passkey");
-        break;
-#endif
-    case ESP_BT_GAP_MODE_CHG_EVT:
-        DT_DEBUG("GAP mode changed: %d", param->mode_chg.mode);
         break;
     default:
         break;
     }
 }
 
+static void bluetooth_hid_cb(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param) {
+    switch (event) {
+    case ESP_HIDD_EVENT_REG_FINISH:
+        if (param->init_finish.state == ESP_HIDD_INIT_OK) {
+            DT_INFO("HID registration success");
+            esp_ble_gap_set_device_name(device_name);
+            esp_ble_gap_config_adv_data(&adv_data);
+        } else {
+            DT_ERROR("HID registration failed");
+        }
+        break;
+    case ESP_HIDD_EVENT_BLE_CONNECT:
+        DT_INFO("BLE device connected");
+        if (device_state_cb) {
+            device_state_cb(BT_DEVICE_CONNECTED);
+        }
+        break;
+    case ESP_HIDD_EVENT_BLE_DISCONNECT:
+        DT_INFO("BLE device disconnected");
+        if (device_state_cb) {
+            device_state_cb(BT_DEVICE_DISCONNECTED);
+        }
+        DT_INFO("Restarting advertising");
+        esp_ble_gap_start_advertising(&adv_params);
+        break;
+    default:
+        break;
+    }
+}
+
+void bluetooth_set_device_state_cb(bluetooth_device_state_cb_t cb) {
+    device_state_cb = cb;
+}
+
 int bluetooth_init(void) {
     esp_err_t ret;
+
+    if ((ret = esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+        DT_ERROR("Failed to release unused BT classic memory: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
+        return 1;
+    }
 
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
@@ -63,7 +119,7 @@ int bluetooth_init(void) {
         return 1;
     }
 
-    if ((ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM)) != ESP_OK) {
+    if ((ret = esp_bt_controller_enable(ESP_BT_MODE_BLE)) != ESP_OK) {
         DT_ERROR("Failed to enable controller: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
         return 1;
     }
@@ -78,34 +134,30 @@ int bluetooth_init(void) {
         return 1;
     }
 
-    if ((ret = esp_bt_gap_register_callback(bluetooth_gap_cb)) != ESP_OK) {
+    if ((ret = esp_ble_gap_register_callback(bluetooth_gap_cb)) != ESP_OK) {
         DT_ERROR("Failed to register GAP callback: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
         return 1;
     }
 
-    if ((ret = esp_bt_hid_device_register_callback(bluetooth_hid_cb)) != ESP_OK) {
-        DT_ERROR("Failed to register HID device callback: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
+    if ((ret = esp_hidd_profile_init()) != ESP_OK) {
+        DT_ERROR("Failed to initialize HID profile: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
         return 1;
     }
 
-    if ((ret = esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_VARIABLE, 0, NULL)) != ESP_OK) {
-        DT_ERROR("Failed to set GAP PIN parameters: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
-        return 1;
-    }
-
-#ifdef CONFIG_BT_SSP_ENABLED
-    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_NONE;
-    if ((ret = esp_bt_gap_set_security_param(ESP_BT_SP_IOCAP_MODE, &iocap, sizeof(iocap))) != ESP_OK) {
-        DT_ERROR("Failed to set SSP parameters: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
-        return 1;
-    }
-#endif
-
-    esp_bt_cod_t cod;
-    cod.major = ESP_BT_COD_MAJOR_DEV_PERIPHERAL;
-    if ((ret = esp_bt_gap_set_cod(cod, ESP_BT_SET_COD_MAJOR_MINOR)) != ESP_OK) {
-        DT_WARNING("Failed to set Class of Device: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
-    }
+    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;     //bonding with peer device after authentication
+    esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;           //set the IO capability to No output No input
+    uint8_t key_size = 16;      //the key size should be 7~16 bytes
+    uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size, sizeof(uint8_t));
+    /* If your BLE device act as a Slave, the init_key means you hope which types of key of the master should distribute to you,
+    and the response key means which key you can distribute to the Master;
+    If your BLE device act as a master, the response key means you hope which types of key of the slave should distribute to you,
+    and the init key means which key you can distribute to the slave. */
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
 
     initialized = true;
     return 0;
@@ -126,8 +178,8 @@ int bluetooth_start(void) {
     }
 
     if (device_name == NULL) {
-        const int id_len = 8;
-        char template[] = "DeskThing-Unknown-";
+        const int id_len = 4;
+        char template[] = "DT-";
         char name[sizeof(template) + id_len];
         strcpy(name, template);
         int i;
@@ -140,22 +192,7 @@ int bluetooth_start(void) {
         bluetooth_set_device_name(name);
     }
 
-    esp_err_t ret;
-
-    if ((ret = esp_bt_dev_set_device_name(device_name)) != ESP_OK) {
-        DT_WARNING("Failed to set device name: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
-    }
-
-    if ((ret = esp_bt_hid_device_init()) != ESP_OK) {
-        DT_ERROR("Failed to initialize HID device: " DT_ESP_ERR_FORMAT, DT_ESP_ERR(ret));
-        return 1;
-    }
-
-    BaseType_t err;
-    if ((err = bluetooth_start_autoreconnect_task()) != pdPASS) {
-        DT_ERROR("Failed to start bluetooth autoreconnect task, error code: %d", err);
-        return 1;
-    }
+    esp_hidd_register_callbacks(bluetooth_hid_cb);
 
     return 0;
 }
