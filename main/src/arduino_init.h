1113d63b619f52d42a4c685c0c6d739761fd4563
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-11-23 15:29:53 +0100

    Don't disable bluetooth in arduino initialization.

---
+++ main/src/arduino_init.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef _ARDUINO_INIT_H_INCLUDED
+#define _ARDUINO_INIT_H_INCLUDED
+
+void initArduinoWithBT();
+
+#endif
--- main/src/override-arduino-bluetooth.c
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-
-// for some reason arduino core decides it's a good idea to free bluetooth
-// memory if this function returns false, which apparently is overriden in
-// esp32-hal-bt.c but it doesn't work
-bool btInUse() { return true; }
