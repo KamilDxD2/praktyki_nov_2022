3401244f83aa696f7d8b1e7233e977cee5edef43
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-11-24 09:26:12 +0100

    Add extern C to header.

--- main/src/arduino_init.h
+++ main/src/arduino_init.h
@@ -3,6 +3,14 @@
 #ifndef _ARDUINO_INIT_H_INCLUDED
 #define _ARDUINO_INIT_H_INCLUDED
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void initArduinoWithBT();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
